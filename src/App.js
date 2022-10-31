49fa315a4791e333f18e62feb52656c99ebf499e
msu580010 <85040048+msu580010@users.noreply.github.com>
2022-10-31 09:54:58 -0400

    commented category

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Home from "./components/Home";
 function App() {
   return (
     <div className="App">
-      {/* <Home/> */}
-      <Categories/>
+      <Home/>
+      {/* <Categories/> */}
     </div>
   );
 }

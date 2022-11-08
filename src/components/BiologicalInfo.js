4941bf4411bfd57f84c08596a64044a1f4c0f638
msu580010 <85040048+msu580010@users.noreply.github.com>
2022-11-08 13:36:16 -0500

    Added attributes for biological information

--- src/components/BiologicalInfo.js
+++ src/components/BiologicalInfo.js
@@ -1,15 +1,46 @@
 import React, { Component, useState }  from 'react';
 import {dogList} from '../dogList.json';
 import { LoremIpsum } from 'react-lorem-ipsum';
+import DogIcon from './DogIcon';
 
 const BiologicalInfo = () => {
-    return (
-    <div>
-        BiologicalInfo
-    </div>
-    
+  return (
+    <div className='displayBody'>
+      <div className='displayIcon'></div>
+         <div className='boxContent'>
+             <h2 className='dogName'>DOGNAME</h2> 
+             <div className='dogTitle'>
+              <div>
+                  <h4 className='origin'>Origin</h4>
+                  <LoremIpsum p={1}/>
+              </div>
+              <div>
+                  <h4 className='height'>Height</h4>
+                  <LoremIpsum p={1}/>
+              </div>
+              <div>
+                  <h4 className='weight'>Weight</h4>
+                  <LoremIpsum p={1}/>
+              </div>
+              <div>
+                  <h4 className='lifespan'>Life Span</h4>
+                  <LoremIpsum p={1}/>
+              </div>
+              <div>
+                  <h4 className='healthissue'>Health Issues</h4>
+                  <LoremIpsum p={1}/>
+              </div>
+              <div>
+                  <h4 className='blurb'>Blurb</h4>
+                  <LoremIpsum p={1}/>
+              </div>
+            </div>
+            
+         </div>
+      </div>
 
-  )
+
+)
 }
 
 export default BiologicalInfo
--- src/components/History.js
+++ src/components/History.js
@@ -1,14 +1,22 @@
 import React, { Component, useState }  from 'react';
 import {dogList} from '../dogList.json';
 import { LoremIpsum } from 'react-lorem-ipsum';
+import DogIcon from './DogIcon';
 
 const History = () => {
 
     return (
-    <div>
-        History
-    </div>
-    
+      <div className='displayBody'>
+        <div className='displayIcon'></div>
+           <div className='boxContent'>
+               <h2 className='dogName'>DOGNAME</h2> 
+               <div className='dogTitle'>
+                   <LoremIpsum p={6} />
+               </div>
+              
+           </div>
+        </div>
+
 
   )
 }

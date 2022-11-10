472580ec509fd025fc743bf34f23b9afbce43b8a
Brandon England <113310196+Oclas@users.noreply.github.com>
2022-11-09 19:15:39 -0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
             <div class="welcome">
                 <h1 class="title1">WELCOME TO</h1>
                 <h1 class="title2">DOGALLERY</h1>
-                <p className="homeMessage">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore, a id! Cum nostrum dolorem sit, atque minima exercitationem odit, vero fugiat sunt corrupti ea ut voluptates adipisci totam vitae nisi!</p>        
+                <p className="homeMessage">Hello and welcome to Dogallery,a place where you can find information on different dog breeds and get a better idea of their size and appearance through the use of 3D models!</p>        
                 <a href="/categories" className="exploreButton">Explore Dog Breeds <TfiArrowRight/></a>
             </div>  
         </div>

130da6633e70055f22d333dc797cf1b79a6927c5
msu580010 <85040048+msu580010@users.noreply.github.com>
2022-11-10 11:20:23 -0500

    fixed about and contact screen size

--- src/components/About.js
+++ src/components/About.js
@@ -3,11 +3,12 @@ import {LoremIpsum} from 'react-lorem-ipsum';
 const About = () => {
   return (
     <div className='About'>
+     
+
       <h1 className='aboutTitle'>ABOUT US</h1>
       <h1 className='aboutUnderline'>________________</h1>
-      <p className='aboutInfo'><LoremIpsum p={6} /></p>
-      <div className='aboutCircle'>
-      </div>
+      <p className='aboutInfo'><LoremIpsum p={3} /></p>
+      <div className='aboutCircle'></div>
     </div>
   )
 }

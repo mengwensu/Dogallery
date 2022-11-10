697fa8667510b3b6ec4d811b4601916143c43430
msu580010 <85040048+msu580010@users.noreply.github.com>
2022-11-10 11:14:54 -0500

    youtube video embedded

--- src/components/Video.js
+++ src/components/Video.js
@@ -6,7 +6,16 @@ const Video = () => {
 
     return (
     <div>
-        Video
+        <iframe className='video'
+          width="1000" 
+          height="500" 
+          src="https://www.youtube.com/embed/Pr68R_M5FVk" 
+          title="YouTube video player" 
+          frameborder="0" 
+
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+          allowfullscreen>
+        </iframe>
     </div>
     
 
